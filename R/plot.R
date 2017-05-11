e72bf7776d2feb26d88721bb21151bdf9a5a7fd1
sobirot <sobi@dtu.dk>
2017-05-11 15:04:25 +0200

    New version of the plot function

--- R/plot.R
+++ R/plot.R
@@ -1,74 +1,210 @@
-plotTemplate<-function(data,data.opt=NA, trace,xrange,yrange,xtitle,ytitle){
-  p <-plot_ly()%>%
-    add_trace(x = eval(parse(text=paste0("data$",as.name(trace$first$x)))),
-              y = eval(parse(text=paste0("data$",as.name(trace$first$y)))),
-              name = trace$first$name,text=~ifelse(is.na(trace$first$text),"", trace$first$text),
-              type = 'scatter', mode = 'lines+markers'
-    )%>%
-    add_trace(x=eval(parse(text=paste0("data.opt$",as.name(trace$opt$x)))),
-              y=eval(parse(text=paste0("data.opt$",as.name(trace$opt$y)))),
-              type = 'scatter', mode = 'lines+markers',name=trace$opt$name,
-              hoverinfo='skip y + x + text',text = ifelse(is.na(trace$opt$text),"", trace$opt$text)
-    )%>%
-    layout(
-      xaxis = list(title = xtitle,range = ytitle),
-      yaxis = list(title = ytitle,range = yrange)
-      ,hovermode = 'x'
+plotTemplate<-function(data,xtitle,ytitle,title,annotation){
+  
+  min.range=apply(matrix(sapply(data,min),2,dim(data)[2]),1,min)
+  max.range=apply(matrix(sapply(data,max),2,dim(data)[2]),1,max)
+  
+  p <- plot_ly()%>%
+    layout(title = title,
+           xaxis = list(title = xtitle,range = c(0.95*min.range[1],1.05*max.range[1])),
+           yaxis = list(title = ytitle,range = c(0.95*min.range[2],1.05*max.range[2]))
+           ,hovermode = 'y',annotations=annotation
     )
+  
+  for (i in 1: dim(data)[2]){
+    p <- add_trace(p,x=data[,i][[1]],y=data[,i][[2]],type = "scatter", mode = 'lines+markers',name =colnames(data)[i])
+    
+  }
   return(p)
 }
 
-discrimPwrCurve <-  function(pdA, pd0 = 0, alpha = 0.05, pGuess = 1/2, 
-                             test = c("difference", "similarity"), 
-                             statistic = c("exact", "normal", "cont.normal"),
-                             SS=c("first","stable"))
+power.SS=function(pdA, pd0, alpha, target.power,pGuess,sample.size,statistic,test){
+  power <- seq(0.51,0.99,0.01) #power>0.5 for discrimSS
+  ss <- sapply(power, function(s) {discrimSS(pdA=pdA, pd0 = pd0,alpha = alpha, target.power=s,pGuess=pGuess,statistic = statistic,test =test)})
+  return(list(ss=ss,power=power))
+}
+power.ES=function(pdA, pd0, alpha, target.power,pGuess,sample.size,statistic,test){
+  ES <- seq(0.01,0.99,0.01)
+  power <- sapply(ES, function(s) {discrimPwr(pdA=s, pd0 = pd0,alpha = alpha, sample.size=sample.size,pGuess=pGuess,statistic = statistic,test =test)})
+  return(list(ES=ES,power=power))
+}
+SS.ES=function(pdA, pd0, alpha, target.power,pGuess,sample.size,statistic,test){
+  ES <- seq(0.51,0.99,0.01) #power>0.5 for discrimSS
+  ss <- sapply(ES, function(s) {discrimSS(pdA=s, pd0 = pd0,alpha = alpha, target.power=target.power,pGuess=pGuess,statistic = statistic,test =test)})
+  return(list(ES=ES,ss=ss))
+}
+
+discrimCalc <-  function(type=c("power-SS","power-ES","SS-ES"),display=c("protocol","alpha","ES","power","SS")
+                         ,pdA=c(0.25,0.375,0.5), pd0 = 0, target.power = c(0.9,0.85,0.8), alpha = c(0.05,0.10,0.01), pGuess = c(1/3,1/2,1/9,1/4), sample.size=c(20,50,100), 
+                         test = c("difference", "similarity"), 
+                         statistic = c("exact","stable.exact", "normal", "cont.normal"))
 {
   
-  ss <- c(5:150)
-  vecPower <- sapply(ss, function(s) discrimPwr(pdA=pdA, pd0 = pd0, sample.size=s,pGuess=pGuess))
-  lag.vecPower <- c(tail(vecPower,-1),T)
+  ## power-SS curve
+  if (type =="power-SS"){
+    if (display=="protocol"){
+      #vec=pGuess
+      data <- sapply(pGuess, function(s) power.SS(pdA=pdA, pd0=pd0, alpha = alpha, target.power=target.power,pGuess=s,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("pGuess = ",fractions(pGuess),sep="")
+      text.anot=paste("pdA = ", round(pdA*100,1),"%<br>alpha = ",round(alpha*100,0), "%<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),""))
+      xtitle="Sample size"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="alpha"){
+      #vec=alpha
+      data <- sapply(alpha, function(s) power.SS(pdA=pdA, pd0=pd0,alpha =s, target.power=target.power,pGuess=pGuess,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("alpha = ",round(alpha*100,0),"%",sep="")
+      text.anot=paste("pdA = ", round(pdA*100,1),"%<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Sample size"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="ES"){
+      #vec=pdA
+      data <- sapply(pdA, function(s) power.SS(pdA=s, pd0=pd0,alpha =alpha, target.power=target.power,pGuess=pGuess,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("pdA = ",round(pdA*100,1),"%",sep="")
+      text.anot=paste("alpha = ", round(alpha*100,0)," %<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Sample size"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }
+    
+    anot=list(
+      text=text.anot,
+      align = "left",
+      bordercolor = "black",
+      xref= "paper",
+      yref="paper",
+      x=1,
+      y=0,
+      xanchor = "right",
+      yanchor = "bottom",
+      showarrow=F
+    )
+  }
+  else if (type =="power-ES"){
+    if (display=="protocol"){
+      #vec=pGuess
+      data <- sapply(pGuess, function(s) power.ES(pdA=pdA, pd0=pd0, alpha = alpha, target.power=target.power,pGuess=s,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("pGuess = ",fractions(pGuess),sep="")
+      text.anot=paste("SS = ", round(sample.size,0),"<br>alpha = ",round(alpha*100,0), "%<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),""))
+      xtitle="Effect size (pdA)"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="alpha"){
+      #vec=alpha
+      data <- sapply(alpha, function(s) power.ES(pdA=pdA, pd0=pd0,alpha =s, target.power=target.power,pGuess=pGuess,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("alpha = ",round(alpha*100,0),"%",sep="")
+      text.anot=paste("SS = ", round(sample.size,0),"<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Effect size (pdA)"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="SS"){
+      #vec=sample.size
+      data <- sapply(sample.size, function(s) power.ES(pdA=pdA, pd0=pd0,alpha =alpha, target.power=target.power,pGuess=pGuess,sample.size=s,statistic = statistic,test=test))
+      colnames(data)=paste("SS = ",round(sample.size,0),sep="")
+      text.anot=paste("alpha = ", round(alpha*100,0),"%<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Effect size (pdA)"
+      ytitle="Power"
+      title=paste ("Power of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }
+    anot=list(
+      text=text.anot,
+      align = "left",
+      bordercolor = "black",
+      xref= "paper",
+      yref="paper",
+      x=1,
+      y=0,
+      xanchor = "right",
+      yanchor = "bottom",
+      showarrow=F
+    )
+  }  else if (type =="SS-ES"){
+    if (display=="protocol"){
+      #vec=pGuess
+      data <- sapply(pGuess, function(s) SS.ES(pdA=pdA, pd0=pd0, alpha = alpha, target.power=target.power,pGuess=s,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("pGuess = ",fractions(pGuess),sep="")
+      text.anot=paste("Power = ", round(sample.size,0),"%<br>alpha = ",round(alpha*100,0), "%<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),""))
+      xtitle="Effect size (pdA)"
+      ytitle="Sample Size"
+      title=paste ("Sample size of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="alpha"){
+      #vec=alpha
+      data <- sapply(alpha, function(s) SS.ES(pdA=pdA, pd0=pd0,alpha =s, target.power=target.power,pGuess=pGuess,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("alpha = ",round(alpha*100,0),"%",sep="")
+      text.anot=paste("Power = ", round(sample.size,0),"%<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Effect size (pdA)"
+      ytitle="Sample Size"
+      title=paste ("Sample size of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }else if (display=="power"){
+      #vec=alpha
+      data <- sapply(target.power, function(s) SS.ES(pdA=pdA, pd0=pd0,alpha =alpha, target.power=s,pGuess=pGuess,sample.size=sample.size,statistic = statistic,test=test))
+      colnames(data)=paste("Power = ",round(target.power*100,0),"%",sep="")
+      text.anot=paste("alpha = ", round(alpha*100,0),"%<br>pGuess = ",fractions(pGuess),"<br>statistic = ",statistic,ifelse(test=="similarity",paste("<br>pd0 = ", round(pd0*100,1), "%"),"") )
+      xtitle="Effect size (pdA)"
+      ytitle="Sample Size"
+      title=paste ("Sample size of",ifelse(test=='difference', 'difference',ifelse(test=='similarity','similarity')), "test")
+    }
+    anot=list(
+      text=text.anot,
+      align = "left",
+      bordercolor = "black",
+      xref= "paper",
+      yref="paper",
+      x=1,
+      y=1,
+      xanchor = "right",
+      yanchor = "top",
+      showarrow=F
+    )
+  }
+  return(list(data=data,xtitle=xtitle,ytitle=ytitle,title=title, anot=anot))
   
-  #Checking the growth of the zig-zag curve
-  subsetPower=vecPower[vecPower>lag.vecPower]
-  subsetSS=ss[vecPower>lag.vecPower]
-  
-  #Stable sample size
-  stablePower=subsetPower[c(diff(subsetPower)>0,T)]
-  stableSS=subsetSS[c(diff(subsetPower)>0,T)]
-  
-  #First exact sample size
-  firstPower=subsetPower[c(T,diff(subsetPower)>0)]
-  firstSS=subsetSS[c(T,diff(subsetPower)>0)]
-  
-  #Number of discriminator
-  nb.discrim=pdA*subsetSS
-  nb.discrimStable=nb.discrim[c(diff(subsetPower)>0,T)]
-  nb.discrimFirst=nb.discrim[c(T,diff(subsetPower)>0)]
+}
 
-  #Number of correct answer
-  nb.correct=pd2pc(pdA,pGuess)*subsetSS
-  nb.correctStable=nb.correct[c(diff(subsetPower)>0,T)]
-  nb.correctFirst=nb.correct[c(T,diff(subsetPower)>0)]
+discrimPlot=function(type=c("power-SS","power-ES","SS-ES"),display=c("protocol","alpha","ES","power","SS")
+                     ,pdA=c(0.25,0.375,0.5), pd0 = 0, target.power = c(0.9,0.85,0.8), alpha = c(0.05,0.10,0.01), pGuess = c(1/3,1/2,1/9,1/4), sample.size=c(20,50,100), 
+                     test = c("difference", "similarity"), 
+                     statistic = c("exact","stable.exact", "normal", "cont.normal")){
+  test <- match.arg(test)
+  stat <- match.arg(statistic)
+  stopifnot(is.numeric(pdA)&& pdA >= 0 && pdA <= 1)
+  stopifnot(is.numeric(pd0) && length(pd0) == 1 && pd0 >= 0 && pd0 <= 1)
+  stopifnot(is.numeric(alpha) && alpha > 0 && alpha < 1)
+  stopifnot(is.numeric(target.power) && target.power > 0 && target.power < 1)
+  stopifnot(is.numeric(pGuess) && pGuess >= 0 && pGuess < 1)
+  stopifnot(is.numeric(sample.size) && isTRUE(all.equal(round(sample.size), sample.size)) && sample.size > 0)
+  sample.size <- as.integer(round(sample.size))
   
-  #Data frame for plotting with plot_ly
-  data.stable=cbind.data.frame(SS=stableSS,power=stablePower,nb.correct=nb.correctStable,nb.discrim=nb.discrimStable)
-  data.first=cbind.data.frame(SS=firstSS,power=firstPower,nb.correct=nb.correctFirst,nb.discrim=nb.discrimFirst)
-  data=cbind.data.frame(SS=ss,power=vecPower,nb.correct=pd2pc(pdA,pGuess)*ss,nb.discrim=pdA*ss)
+  if (type=="power-SS" && !display%in%c("protocol","alpha","ES") )
+    stop("type = power-SS: display has be chosen among protocol, alpha and ES")
+  if (type=="power-ES" && !display%in%c("protocol","alpha","SS") )
+    stop("type = power-ES: display has be chosen among protocol, alpha and SS")
+  if (type=="SS-ES" && !display%in%c("protocol","alpha","power") )
+    stop("type = SS-ES: display has be chosen among protocol, alpha and power")
   
-  #Data to input to the plot_ly template                  ))
-  input=NULL
+  if(display=="ES") pdA else pdA=pdA[1]
+  if(display=="protocol") pGuess else pGuess=pGuess[1]
+  if(display=="power") target.power else target.power=target.power[1]
+  if(display=="alpha") alpha=alpha[order(alpha,decreasing = T)] else alpha=alpha[1]
+  if(display=="SS") sample.size else sample.size=sample.size[1]
+  statistic = ifelse(length(statistic)>1,statistic[1],statistic)
+  test = ifelse(length(test)>1,test[1],test)
   
-  input$first$x=names(data)[1]
-  input$first$y=names(data)[2]
-  input$first$name='Sample size'
-  input$first$text=NA
+  if(type=="power-SS"&&test == "difference" && any(pdA <= pd0))
+    stop("type = power-SS: pdA has to be larger than pd0 for difference tests")
+  if(type=="power-SS"&&test == "similarity" && any(pdA >= pd0))
+    stop("type = power-SS: pdA has to be less than pd0 for similarity tests")
   
-  data.opt=suppressWarnings(if (SS=="first") data.first else data.stable)
-  input$opt$x=names(data.opt)[1]
-  input$opt$y=names(data.opt)[2]
-  input$opt$name=suppressWarnings(if (SS=="first") "First exact" else "Stable exact")
-  input$opt$text=with(data.opt,paste('Power: ', round(power*100,1),'%','</br> Nb. discriminator:',nb.discrim,'</br> Nb. correct answer:',nb.correct))
-  p=plotTemplate(data=data,data.opt=data.opt, trace=input,xrange=ss,yrange=c(0,1),xtitle='Sample size',ytitle='Power')
-
-  return(plot=p)
+  if(type=="power-ES"&&test == "difference" && any(pd0 > 0))
+    stop("type = power-ES: 0 < pdA < 1,  pd0 has to be less than pdA for difference tests")
+  if(type=="power-ES"&&test == "similarity" && any(pd0 < 1))
+    stop("type = power-ES: 0 < pdA < 1, pd0 has to be larger than pdA for similarity tests")
+  
+  if(type=="SS-ES"&&test == "difference" && any(pd0 > 0.5))
+    stop("type = SS-ES: 0.5 < pdA < 1,  pd0 has to be less than pdA for difference tests")
+  if(type=="SS-ES"&&test == "similarity" && any(pd0 < 1))
+    stop("type = SS-ES: 0.5 < pdA < 1, pd0 has to be larger than pdA for similarity tests")
+  
+  input=discrimCalc(type = type, display =display, pdA=pdA,pd0=pd0,target.power = target.power, alpha = alpha,pGuess = pGuess, sample.size = sample.size, test = test, statistic = statistic)
+  plotTemplate(data=input$data,xtitle=input$xtitle,ytitle=input$ytitle,title=input$title,annotation=input$anot)
 }
